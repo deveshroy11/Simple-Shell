b29c613f3f7c2b5af7afe4f3b62cc5d13672cea7
deveshroy11 <162672735+deveshroy11@users.noreply.github.com>
2024-09-24 20:11:59 +0530

    Update handle_bg.c

--- handle_bg.c
+++ handle_bg.c
@@ -5,6 +5,10 @@
 void handle_sigstp(int sig){
     printf("Process stopped\n");
 }
+//to handle ctrl+c
+void handle_second(int sig){
+    printf("Process interuppted");
+}
 
 int main(int argc,char* argv[]){
     struct sigaction z;
@@ -12,6 +16,9 @@ int main(int argc,char* argv[]){
     z.sa_flags=SA_RESTART;
     sigaction(SIGTSTP,&z,NULL);
 
+// no need to set the flags again it remains the same when set once
+    z.sa_handler=&handle_second;
+    sigaction(SIGINT,&z,NULL);
     return 0;
 
 
